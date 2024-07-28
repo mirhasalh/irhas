ef909f69a277f08e14e92f7f520802ec844fc524
Irhas <muhammadirhasalh@gmail.com>
2024-07-28 20:45:37 +0700

    Updated themes

--- src/lib/components/Article.svelte
+++ src/lib/components/Article.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
+  import { goto } from '$app/navigation'
   import { isMobile } from '$stores/isMobile'
   import { format, parseISO } from 'date-fns'
 
   export let post: any
 </script>
 
-<article class={`blog-post`} class:sm={$isMobile}>
+<article role="presentation" class={`blog-post`} class:sm={$isMobile} on:click|preventDefault={() => goto(`/post/${post.slug}`)} on:keyup={() => goto(`/post/${post.slug}`)}>
   <div class={`leading`}>
     <time datetime={post.date}>{format(new Date(parseISO(post.date)), `${$isMobile ? 'MMM d, yyyy' : 'MMMM d, yyyy'}`)}</time>
     <br />
@@ -14,9 +15,11 @@
   <div class={`preview`}>
     <h3>{post.title}</h3>
     {#if `${post.src}` !== 'n/a'}
-      <img src={post.src} alt={post.title}>
+      <div class={`image`}>
+        <img src={post.src} alt={post.title} />
+      </div>
     {/if}
-    <small>{@html post.preview.html}</small>
+    {@html post.preview.html}
     <a href={`/post/${post.slug}`} data-sveltekit-prefetch><strong>Read →</strong></a>
   </div>
 </article>
