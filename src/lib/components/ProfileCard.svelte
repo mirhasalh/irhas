58bf4e76ae9e69bd8ee662560921599d2f765131
Irhas <muhammadirhasalh@gmail.com>
2024-09-28 09:55:27 +0700

    Replaced png with webp

--- src/lib/components/ProfileCard.svelte
+++ src/lib/components/ProfileCard.svelte
@@ -7,7 +7,7 @@
 <section class={`main-padding`}>
   <div class={`card`}>
     <div class={`banner`}>
-      <img src={`./IMG_20240825-083515.png`} alt={`Irhas's blog post banner`} />
+      <img src={`./IMG_20240825-083515.webp`} alt={`Irhas's blog post banner`} />
       <div class={`avatar-wrapper`}>
         <div class={`circular-avatar`} role="presentation" on:click={() => onAvatar()}>
           <img src={avatar} {alt} />
