ac94ded48e2d616ffdb540ef58e0b3ddc50b6222
Irhas <muhammadirhasalh@gmail.com>
2024-12-18 17:07:34 +0700

    fix(headings): use regex as a workaround

--- src/routes/post/[slug]/+page.server.ts
+++ src/routes/post/[slug]/+page.server.ts
@@ -1,10 +1,9 @@
 import type { PageLoad } from './$types'
 import { compile } from 'mdsvex'
-import { getPosts, getPost, parseCompiled } from '$lib'
+import { getPosts, getPost, parseCompiled, getHeadingIds } from '$lib'
 import { rehype } from 'rehype'
 import rehypeSlug from 'rehype-slug'
 import rehypeAutolinkHeadings from 'rehype-autolink-headings'
-import { parseDocument } from 'htmlparser2'
 
 export const load: PageLoad = async ({ params }: { params: { slug: string } }) => {
   const post = await getPost(params.slug)
@@ -19,25 +18,10 @@ export const load: PageLoad = async ({ params }: { params: { slug: string } }) =
     processedHtml = String(file)
   }
 
-  const headings: string[] = [],
-    document = parseDocument(processedHtml)
-
-  function traverse(node: any) {
-    if (node.tagName && /^h[1-6]$/i.test(node.tagName)) {
-      const id =
-        node.attribs?.id ||
-        node.children?.[0]?.data
-          ?.trim()
-          .toLowerCase()
-          .replace(/\s+/g, '-')
-
-      if (id) headings.push(id)
-    }
-
-    if (node.children) node.children.forEach(traverse)
+  return {
+    posts: await getPosts(0, 5),
+    post,
+    code: processedHtml,
+    headings: getHeadingIds(processedHtml)
   }
-
-  traverse(document)
-
-  return { posts: await getPosts(0, 5), post, code: processedHtml, headings }
 }
