246b9981aae1950b5eb6fac0feac35fc5ff2fb75
Irhas <muhammadirhasalh@gmail.com>
2025-01-08 21:02:41 +0700

    feat(recent-work): add link

--- src/lib/components/RecentWork.svelte
+++ src/lib/components/RecentWork.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
   import { recentWork } from '$lib/info'
+  import AppLink from './AppLink.svelte'
 </script>
 
 <h1 class={`animated-fade-in`}>Recent work</h1>
 <ul class={`list`}>
   {#each recentWork as w}
     <li role={`listitem`} class={`list-item animated-fade-in`}>
-      <span>
-        <strong>{w.year}</strong>
-        <br />
-        {w.title}
-      </span>
+      {#if w.id === 4}
+        <AppLink text={`${w.title} (${w.year})`} urls={w.urls} />
+      {:else}
+        <a class={`link`} href={w.urls[0]} target="_blank">{`${w.title} (${w.year})`}</a>
+      {/if}
       <figure>
         <img class={`cover`} src={w.imgUrl} alt={w.title} />
         <figcaption>
--- src/lib/info.ts
+++ src/lib/info.ts
@@ -2,37 +2,42 @@ import { website } from "$lib"
 
 export const recentWork = [
     {
+        id: 0,
         year: 2024,
         imgUrl: `${website}/IMG_20250108-174556.webp`,
-        url: 'n/a',
+        urls: ['https://play.google.com/store/apps/details?id=com.dafturn.mypertamina&hl=en'],
         title: 'MyPertamina x UCOllect microsite UI/UX design',
         desc: 'Created the UCOllect microsite using Figma, ensuring a seamless design that integrates cohesively with the MyPertamina app to provide a unified user experience.'
     },
     {
+        id: 1,
         year: 2024,
         imgUrl: `${website}/IMG_20250108-174004.webp`,
-        url: 'n/a',
+        urls: ['https://demo.photobook.ai/'],
         title: 'Photobook AI demo website',
         desc: 'Designed and developed a demo website for a SaaS product using the Svelte 5 JavaScript framework. Delivered the project successfully within an ambitious 4-day deadline 💪.'
     },
     {
+        id: 2,
         year: 2024,
         imgUrl: `${website}/IMG_20250108-172909.webp`,
-        url: 'n/a',
+        urls: ['https://gomining.com/tokenomics'],
         title: 'GoMining landing page',
         desc: 'Served as a Frontend Engineer using the Vue.js framework to build and optimize the landing page for the multinational company GoMining.'
     },
     {
+        id: 3,
         year: 2024,
         imgUrl: `${website}/IMG_20250108-173808.webp`,
-        url: 'n/a',
+        urls: ['https://demo.photobook.ai/'],
         title: 'AI-Powered photo book editor',
         desc: 'Worked as a Frontend Engineer at Photobook.ai in Singapore, specializing in building seamless user experiences with Svelte. Responsibilities included creating and maintaining frontend components, collaborating with cross-functional teams, and enhancing web applications.'
     },
     {
+        id: 4,
         year: 2023,
         imgUrl: `${website}/IMG_20230521-195115.webp`,
-        url: 'n/a',
+        urls: ['https://apps.apple.com/id/app/ucollect-by-noovoleum/id6462969708', 'https://play.google.com/store/apps/details?id=com.noovoleum.ucollect&hl=en'],
         title: 'UCOllect app by noovoleum',
         desc: 'Initiated and executed the design and development of the UCOllect app as the pioneer engineer on the team.'
     }
