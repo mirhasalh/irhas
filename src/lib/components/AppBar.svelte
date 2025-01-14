19a0885c10ed2e06017c99ac77a9177c5bc783d3
Irhas <muhammadirhasalh@gmail.com>
2025-01-14 10:47:17 +0700

    feat(AppBar): prepared routes

--- src/lib/components/AppBar.svelte
+++ src/lib/components/AppBar.svelte
@@ -1,14 +1,39 @@
 <script lang="ts">
   import Accounts from './Accounts.svelte'
+  import AnimatedMenuIcon from './AnimatedMenuIcon.svelte'
   import Logo from './Logo.svelte'
+
+  const routes = ['Home', 'About-me', 'Posts']
+
+  let detailsEl: HTMLDetailsElement,
+    open = $state(false)
+
+  const onToggle = () => (open = detailsEl?.open || false)
 </script>
 
 <header class={`app-bar glass`}>
   <div class={`app-bar-contraint`}>
-    <nav>
+    <div class={`app-bar-leading`}>
+      <details class={`dropdown`} bind:this={detailsEl} ontoggle={onToggle}>
+        <summary class={`btn ghost sm squared`}>
+          <AnimatedMenuIcon active={open} />
+        </summary>
+        <ul class={`menu dropdown-content`}>
+          {#each routes as route}
+            {@const link = route === 'Home' ? '/' : `/${route.toLowerCase()}`}
+            <li><a href={link} class={`link`}><small>{route}</small></a></li>
+          {/each}
+        </ul>
+      </details>
       <a href="/">
         <Logo width={36} height={36} />
       </a>
+    </div>
+    <nav>
+      {#each routes as route}
+        {@const link = route === 'Home' ? '/' : `/${route.toLowerCase()}`}
+        <a class={`link btn sm ghost`} href={link}><small>{route}</small></a>
+      {/each}
     </nav>
     <div class={`app-bar-actions`}>
       <Accounts />
