2f8b7017bc49804792bace262458cf9b58846220
Irhas <muhammadirhasalh@gmail.com>
2025-01-20 16:09:57 +0700

    refactor: indent

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,26 +1,26 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
-		interface Post {
-			title: string
-			excerpt: string
-			categories: string[]
-			tags: string[]
-			imageUrl: string
-			videoUrl: string
-			publishedAt: string
-			slug: string
-			body: string
-			readingTime: number
-			headings: string[]
-		}
-	}
+  namespace App {
+    // interface Error {}
+    // interface Locals {}
+    // interface PageData {}
+    // interface PageState {}
+    // interface Platform {}
+    interface Post {
+	  title: string
+	  excerpt: string
+	  categories: string[]
+	  tags: string[]
+	  imageUrl: string
+	  videoUrl: string
+	  publishedAt: string
+	  slug: string
+	  body: string
+	  readingTime: number
+	  headings: string[]
+    }
+  }
 }
 
-export {};
+export {}
