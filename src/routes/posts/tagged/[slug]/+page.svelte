b686408d65a443e98fdcc82f10f78d0f227253f1
Irhas <muhammadirhasalh@gmail.com>
2025-01-20 10:40:13 +0700

    feat(search): componetize search icon

--- src/routes/posts/tagged/[slug]/+page.svelte
+++ src/routes/posts/tagged/[slug]/+page.svelte
@@ -3,6 +3,7 @@
   import type { PageData } from './$types'
   import { initFadeInAnimation } from '$lib'
   import Post from '$lib/components/Post.svelte'
+  import Search from '$lib/icons/Search.svelte'
 
   let { data }: { data: PageData } = $props(),
     find = $state(data.slug),
@@ -27,9 +28,7 @@
 <section class={`px pt mb`}>
   <form onsubmit={() => goFind(toFind)}>
     <label class={`input mx-auto`}>
-      <svg class={`label-icon`} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor">
-        <path fill-rule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clip-rule="evenodd" />
-      </svg>
+      <span class={`label-icon`}><Search /></span>
       <input type="text" placeholder="Tag" bind:value={find} onkeypress={onKeyPress} />
     </label>
   </form>
