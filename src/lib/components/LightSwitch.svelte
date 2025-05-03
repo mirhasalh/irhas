54f08879268ccc14df90475a7f44b290bd92f89a
Irhas <irhasm@outlook.com>
2025-05-03 17:55:19 +0700

    chore(svelte-reveal): remove svelte reveal

--- src/lib/components/LightSwitch.svelte
+++ src/lib/components/LightSwitch.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
-  import { reveal } from 'svelte-reveal'
   import Dark from '$icons/Dark.svelte'
   import Light from '$icons/Light.svelte'
 
   let { isDark = $bindable(), onChanged, ...props }: { isDark: boolean; onChanged: (event: Event) => void } = $props()
 </script>
 
-<label class="swap swap-rotate btn btn-ghost btn-circle" use:reveal={{ preset: 'blur' }}>
+<label class="swap swap-rotate btn btn-ghost btn-circle">
   <input type="checkbox" onchange={onChanged} bind:checked={isDark} {...props} />
   <Light />
   <Dark />
