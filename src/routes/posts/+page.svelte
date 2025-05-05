201af6b4857721bc14860bb720a61db99d3ae804
Irhas <irhasm@outlook.com>
2025-05-05 22:52:42 +0700

    feat(posts): add filter

--- src/routes/posts/+page.svelte
+++ src/routes/posts/+page.svelte
@@ -4,15 +4,27 @@
 
   let { data }: { data: PageData } = $props()
 
+  let selectedCategory = $state('')
+
   const obj: any = data as any,
-    posts: App.Post[] = obj.posts as App.Post[]
+    posts: App.Post[] = obj.posts as App.Post[],
+    categories = [...new Set(posts.map((v) => v.categories).flat())]
 </script>
 
 <section class="max-w-4xl mx-auto">
-  <h2 class="merriweather text-4xl m-4 font-bold">Recent posts</h2>
+  <h2 class="merriweather text-4xl mx-4 mt-4 font-bold">Recent posts</h2>
+  <form class="filter m-4">
+    <input class="btn btn-square" type="reset" value="×" />
+    {#each categories as c}
+      <input class="btn" type="radio" bind:group={selectedCategory} name="frameworks" aria-label={c} value={c} />
+    {/each}
+  </form>
   <ul class="recent-posts-grid grid gap-4 md:grid-cols-2 px-4">
     {#each posts as post (post.slug)}
-      <li><PostCard {post} /></li>
+      {@const show = !selectedCategory ? true : post.categories.includes(selectedCategory)}
+      {#if show}
+        <li><PostCard {post} /></li>
+      {/if}
     {/each}
   </ul>
 </section>
