3daf21fbbde3b3bae6b733a85c70ad4276e88417
Irhas <irhasm@outlook.com>
2026-05-11 20:55:50 +0700

    feat(Hover3DCard): replace Card3D with daisyUI hover 3D card (#9)

---
+++ src/lib/components/Hover3DCard.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  let { src, alt }: { src: string; alt: string } = $props()
+</script>
+
+<div class="hover-3d">
+  <figure class="rounded-xl">
+    <img {src} {alt} />
+  </figure>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+</div>
