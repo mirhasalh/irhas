88c6db8e0da7a94772cfccbaf55f7f4377aabc06
Irhas <irhasm@outlook.com>
2026-06-18 05:59:46 +0700

    chore: applied 2 tab space

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,58 +1,58 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
-		interface Post {
-			id: string;
-			updatedAt: string;
-			createdAt: string;
-			title: string
-			excerpt: string
-			category: string
-			tags: string[]
-			publishedAt: string
-			slug: string
-			body: string
-			readingTime: number
-		}
-		interface PrivacyDeclaration {
-			title: string
-			text: string
-		}
-		interface MobileAppUrl {
-			title: string
-			slug: string
-			show: boolean
-			url: string
-		}
-		interface MobileApp {
-			id: string
-			description: string
-			effectiveDateOfPrivacy: string
-			featureGraphic: string
-			icon: string
-			privacyDeclarations: PrivacyDeclaration[]
-			production: boolean
-			shortDescription: string
-			slug: string
-			title: string
-			urls: MobileAppUrl[]
-			year: number
-		}
-		interface Work {
-			id: string
-			title: string
-			description: string
-			imgFileName: string
-			year: number
-			urls: string[]
-		}
-	}
+  namespace App {
+    // interface Error {}
+    // interface Locals {}
+    // interface PageData {}
+    // interface PageState {}
+    // interface Platform {}
+    interface Post {
+      id: string;
+      updatedAt: string;
+      createdAt: string;
+      title: string;
+      excerpt: string;
+      category: string;
+      tags: string[];
+      publishedAt: string;
+      slug: string;
+      body: string;
+      readingTime: number;
+    }
+    interface PrivacyDeclaration {
+      title: string;
+      text: string;
+    }
+    interface MobileAppUrl {
+      title: string;
+      slug: string;
+      show: boolean;
+      url: string;
+    }
+    interface MobileApp {
+      id: string;
+      description: string;
+      effectiveDateOfPrivacy: string;
+      featureGraphic: string;
+      icon: string;
+      privacyDeclarations: PrivacyDeclaration[];
+      production: boolean;
+      shortDescription: string;
+      slug: string;
+      title: string;
+      urls: MobileAppUrl[];
+      year: number;
+    }
+    interface Work {
+      id: string;
+      title: string;
+      description: string;
+      imgFileName: string;
+      year: number;
+      urls: string[];
+    }
+  }
 }
 
-export { }
+export {}
